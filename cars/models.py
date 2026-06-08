aed6655c4caa33339b4de6971bc943c249d9dd9a
Imples74 <gekaknyazev@gmail.com>
2026-06-08 16:51:15 +0500

    Add initial Car model

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    brand = models.CharField(
+        max_length=100
+    )
+
+    model = models.CharField(
+        max_length=100
+    )
+
+    year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.brand} {self.model}"
