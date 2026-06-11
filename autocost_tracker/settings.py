4ff1ee41d5bbd95c04828a962f1ef3e83c21b7b4
Imples74 <gekaknyazev@gmail.com>
2026-06-11 18:30:32 +0500

    Add user authentication

--- autocost_tracker/settings.py
+++ autocost_tracker/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'car_list'
-LOGOUT_REDIRECT_URL = 'home'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'
